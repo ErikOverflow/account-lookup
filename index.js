32209072b2e5496972af7ac0e05703d6b3550e8e
Erik <46797334+ErikOverflow@users.noreply.github.com>
2020-08-21 13:16:30 -0500

    Add dotenv package and move port to .env file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const dotenv = require('dotenv');
+dotenv.config();
 
 const v1 = express.Router();
 const v2 = express.Router();
@@ -12,4 +13,4 @@ const ping = require('./services/ping');
 v1.get('/ping', ping);
 v2.get('/ping', (req,res) => res.status(200).json({message: "Successful ping", otherData: "pigs are pink"}));
 
-app.listen(port);
+app.listen(process.env.PORT, () => console.log(`Server is now listening on port: ${process.env.PORT}`));
