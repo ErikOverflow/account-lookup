dd4992effe4f9a07c8b0e5fb88320d2b982525e4
Erik <46797334+ErikOverflow@users.noreply.github.com>
2020-08-21 18:34:59 -0500

    Formatting

--- services/summoner.js
+++ services/summoner.js
@@ -13,28 +13,42 @@ const getSummonerByName = async (region, summonerName) => {
   //Query for the summoner data in MongoDb
   const yesterday = new Date();
   yesterday.setDate(yesterday.getDate() - 1);
-  const query = { name: new RegExp(`^${summonerName}$`, "i"), region, lastModified: {$gte: yesterday}};
-  const findOptions = { projection: {_id: 0 }};
-  let summonerDoc = await db.collection("summoners").findOne(query, findOptions);
+  const query = {
+    name: new RegExp(`^${summonerName}$`, "i"),
+    region,
+    lastModified: { $gte: yesterday },
+  };
+  const findOptions = { projection: { _id: 0 } };
+  try {
+    let summonerDoc = await db
+      .collection("summoners")
+      .findOne(query, findOptions);
+  } catch (err) {
+    throw err;
+  }
   //Check for up-to-date data within 24 hours
-  if(summonerDoc){
-    return summonerDoc
+  if (summonerDoc) {
+    return summonerDoc;
   }
 
   delete query.lastModified;
   //Get new summoner data from Riot
-  const res = await axios.get(
-    config.summonerByNameUrl(region, summonerName),
-    axiosOptions
-  );
+  try {
+    const res = await axios.get(
+      config.summonerByNameUrl(region, summonerName),
+      axiosOptions
+    );
+  } catch (err) {
+    throw err;
+  }
   let summonerData = res.data;
 
   //Upsert the document to the DB
   const update = {
     $set: summonerData,
-    $currentDate: {lastModified: true}
-  }
-  const updateOptions = {upsert: true};
+    $currentDate: { lastModified: true },
+  };
+  const updateOptions = { upsert: true };
   await db.collection("summoners").updateOne(query, update, updateOptions);
   summonerDoc = await db.collection("summoners").findOne(query, findOptions);
 
@@ -46,8 +60,11 @@ const summonerParser = async (req, res) => {
     return res.status(403).json({ error: "Missing summoner name or region." });
   }
 
-  const summonerData = await getSummonerByName(req.query.region.toLowerCase(), req.query.summonerName)
-  
+  const summonerData = await getSummonerByName(
+    req.query.region.toLowerCase(),
+    req.query.summonerName
+  );
+
   return res.status(200).json(summonerData);
 };
 
